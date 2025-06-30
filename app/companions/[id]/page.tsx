8d4825a65778ca126fb2ec02a84e0edcb0709f4b
Shivam-RN <shivamrana1809@gmail.com>
2025-06-30 20:25:51 +0530

    implement home page ui

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      companion session
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className="text-4xl underline">Page</div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+         id="1"
+         name="Neura the Brainy Explorer"
+         topic="Neural Network"
+         subject="Science"
+         duration={45}
+         color="#ffda6e"
+         />
+        
+        <CompanionCard
+         id="2"
+         name="Neura "
+         topic="Neural Network"
+         subject="Science"
+         duration={4}
+         color="#e5d0ff"/>
+        
+        <CompanionCard
+         id="3"
+         name="Neura the "
+         topic="Neural Network"
+         subject="Science"
+         duration={450}
+         color="#BDE7FF"/>
+        
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+         title="Recently Completed sessions" 
+         companions={recentSessions} 
+         classNames="w-2/3 max-lg:w-full"/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
