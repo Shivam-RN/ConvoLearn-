f47cacc2d682a1fd657f11d3865defa996b7d4c5
Shivam-RN <shivamrana1809@gmail.com>
2025-07-06 12:39:00 +0530

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+
+
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
